73b7ce0ffc803a2f86824b8a61639b74b0507c85
Patricia Organ <trishaorgan@Trishas-MacBook-Pro.local>
2016-12-06 20:36:52 -0600

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+#Name
+
+##Description
+
+##Usage
+
+##Badges (like InnerSource, OpenSource)
+
+##Keywords
+
+##Key project management links (i.e. links to Rally stories)
+
+##Documentation links (include 
+
+##[Contributing](Contributing.md)
+
+##[Help Wanted ](helpwanted.md)
+
+##[Getting Started](gettingstarted.md))
+
+##Announcement list location
---
+++ app.js
@@ -0,0 +1,9 @@
+const tenents = ['Programmers share their work with a wide audience, instead of just with a manager or team. In most open source projects, anyone in the world is free to view the code, comment on it, learn new skills by examining it, and submit changes that they think will improve it or customize it to their needs.',
+'New code repositories (branches) based on the project can be made freely, so that sites with unanticipated uses for the code can adapt it. There are usually rules and technical support for re-merging different branches into the original master branch.',
+'People at large geographical distances, at separate times, can work on the same code or contribute different files of code to the same project.',
+'Communication tends to be written and posted to public sites instead of shared informally by word of mouth, which provides a history of the project as well as learning opportunities for new project members.',
+'Writing unit tests becomes a key programming task. a “unit test” is a small test that checks for a particular, isolated behavior such as rejecting incorrect input or taking the proper branch under certain conditions. In open source and inner source, testing is done constantly as changes are checked in, to protect against failures during production runs.']
+
+
+console.log(tenents);
+
